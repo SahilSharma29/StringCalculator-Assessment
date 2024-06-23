11738dad2fed28bbc58a04f02a3b90f5c7e60fa6
Sahil Sharma <sahil.sharma@123789.org>
2024-06-23 13:40:38 +0530

    Implement add function to handle single and multiple numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -2,5 +2,6 @@ export default function add(numbers: string): number {
   if (numbers === '') {
     return 0;
   }
-  return NaN;
+  const numArray = numbers.split(',').map(Number);
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
